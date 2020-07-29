7b38162c8c9e2d26009f4ea5f518d7130e928199
EvidenceN <33920231+EvidenceN@users.noreply.github.com>
2020-07-28 23:58:19 -0700

    assignment 1 is done

--- queue/queue.py
+++ queue/queue.py
@@ -12,17 +12,59 @@ return elements in First In First Out order.
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
+
+PYTHON EXAMPLE https://docs.python.org/2/tutorial/datastructures.html
 """
+
+#Queue using ARRAY as storage
+
+"""
+Why I used deque
+It is also possible to use a list as a queue, where the first element added is the first element retrieved (“first-in, first-out”); however, lists are not efficient for this purpose. While appends and pops from the end of list are fast, doing inserts or pops from the beginning of a list is slow (because all of the other elements have to be shifted by one).
+
+To implement a queue, use collections.deque which was designed to have fast appends and pops from both ends.
+
+"""
+from collections import deque
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = deque([])
     
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def enqueue(self, value):
-        pass
+        return self.storage.append(value)
 
     def dequeue(self):
-        pass
+        return self.storage.popleft()
+
+import sys
+sys.path.append('../')
+
+from singly_linked.singly_linked_list import LinkedList, Node
+
+# implementing queue using linked list
+# Using an a "linked list" to do stack
+
+linked = LinkedList()
+node = Node()
+class Queuelink:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        # get the length of the linked list
+        return len(self.storage)
+
+    def enque(self, value):
+        # add the value to the head
+        node = Node()
+        return node.set_next(value)
+
+    def dequeue(self):
+        # Dequeing first item would be removing the head
+        return self.storage.remove_head()
--- stack/stack.py
+++ stack/stack.py
@@ -2,6 +2,11 @@
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
 
+* Should have the methods: `push`, `pop`, and `len`.
+   * `push` adds an item to the top of the stack.
+   * `pop` removes and returns the element at the top of the stack
+   * `len` returns the number of elements in the stack.
+
 1. Implement the Stack class using an array as the underlying storage structure.
    Make sure the Stack tests pass.
 2. Re-implement the Stack class, this time using the linked list implementation
@@ -9,17 +14,50 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+
+PYTHON EXAMPLE https://docs.python.org/2/tutorial/datastructures.html
 """
+
+# Using an ARRAY to do stack
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        return self.storage.append(value)
 
     def pop(self):
-        pass
+        return self.storage.pop()
+
+import sys
+
+#sys.path.insert(1,r"C:\Users\Samsung\Google Drive (evidencen@evidencen.com)\Data Science Lambda\Unit 5_Computer Science\2 data structures\Data-Structure")
+
+sys.path.append('../')
+
+from singly_linked.singly_linked_list import LinkedList, Node
+
+# Using an a "linked list" to do stack
+linked = LinkedList()
+node = Node()
+class Stacklink:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        # get the length of the linked list
+        return len(self.storage)
+
+    def push(self, value):
+        # add the value to the tail
+        return self.storage.add_to_tail(value)
+
+    def pop(self):
+        # first get the value of the current tail
+        value = node.get_value()
+        next_tail = node.set_next(value)
